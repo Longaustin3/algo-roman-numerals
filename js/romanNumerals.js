732c96988906ec7742eebecf5d189396fdbef96f
Austin Long <longaustin3@gmail.com>
2024-05-09 14:27:39 -0400

    Solution

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,28 @@
-exports.toRoman = function(num) {
+exports.toRoman = function (num) {
+  const romanToArabic = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1,
+  }
 
+  let romanNumeral = ""
+  for (let numeral in romanToArabic) {
+    let arabicNum = romanToArabic[numeral]
+    let timesRepeated = Math.floor(num / arabicNum)
+    for (let i = 0; i < timesRepeated; i++) {
+      romanNumeral += numeral
+      num -= arabicNum
+    }
+  }
+  return romanNumeral
 };
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -3,3 +3,7 @@ var rn = require("./romanNumerals");
 console.log(rn.toRoman(1) === 'I');
 console.log(rn.toRoman(3) === 'III');
 console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(94) === 'XCIV');
+console.log(rn.toRoman(1234) === 'MCCXXXIV');
+
+
