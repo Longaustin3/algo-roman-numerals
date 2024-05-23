79262c7c2c92a3c631f9df48780c17c7bfd12f85
[Austin Long] <[longaustin3@gmail.com]>
2024-05-23 07:57:30 -0400

    solved in python with two additional tests in spec file

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,29 @@
 def to_roman(num):
     # write your code here!
+    # list of the roman numeral in a dictionary
+    roman_to_arabic = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+    
+    roman_numeral = ""
+    for numeral in roman_to_arabic:
+        arabic_number = roman_to_arabic[numeral]
+        times_repeated = num // arabic_number
+        for time in range (0, times_repeated): 
+            roman_numeral += numeral
+            num -= arabic_number
+
+    return roman_numeral
+
--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -3,4 +3,6 @@ from roman_numerals import to_roman
 print(to_roman(1) == 'I')
 print(to_roman(3) == 'III')
 print(to_roman(4) == 'IV')
+print(to_roman(94) == 'XCIV')
+print(to_roman(1234) == 'MCCXXXIV')
 # add tests to cover different edge cases
